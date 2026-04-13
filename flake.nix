34861f667134fadfb396623af5ae6bad99d77a8c
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-13 17:29:51 +0200

    tmp test

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "CHANGEME";
+  description = "tests";
 
   inputs = {
-    gepetto.url = "github:gepetto/nix";
+    gepetto.url = "github:nim65s/gepetto-nix/test-shell";
     flake-parts.follows = "gepetto/flake-parts";
     systems.follows = "gepetto/systems";
   };
@@ -17,17 +17,26 @@
           inputs.gepetto.flakeModule
           {
             flakoboros = {
-              overrideAttrs.CHANGEME = _: {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./CHANGEME
-                  ];
+              pyOverrideAttrs.eigenpy =
+                { drv-final, drv-prev, ... }:
+                {
+                  version = "9.9.9";
+                  postPatch = ''
+                    substituteInPlace package.xml \
+                      --replace-fail ${drv-prev.version} ${drv-final.version}
+                  '';
                 };
-              };
             };
           }
         ];
+        perSystem =
+          { pkgs, system, ... }:
+          {
+            devShells = {
+              inherit (inputs.gepetto.devShells.${system}) up;
+              down = pkgs.callPackage "${inputs.gepetto}/shell.nix" { name = "down"; };
+            };
+          };
       }
     );
 }
